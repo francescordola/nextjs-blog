64c852a833ff78c0f6cde409ee8b8709be8ca2b9
francescordola <fcordola92@gmail.com>
2022-01-20 16:53:26 +0100

    Next.js - Dynamic Routes + API Routes

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 import Head from 'next/head'
-import Layout, { siteTitle } from '../layout/layout'
+import Layout, { siteTitle } from '../components/layout/layout'
 import utilStyles from '../styles/utils.module.scss'
-import Link from 'next/link'
 import { getSortedPostsData } from '../lib/posts'
+import Link from 'next/link'
+import Date from '../components/date'
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData()
@@ -39,11 +40,13 @@ export default function Home ({ allPostsData }) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+              <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
